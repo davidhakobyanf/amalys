99caeeff5b129b5e7e8668999162e2c4b8c7eb1c
https://github.com/davidhakobyanf/auro.git <hakobyan.david2003@gmail.com>
2025-03-06 10:51:21 +0300

    new

--- src/app/_components/BlockCloudContainer/BlockCloudContainer.tsx
+++ src/app/_components/BlockCloudContainer/BlockCloudContainer.tsx
@@ -1,6 +1,8 @@
 import React, { ElementType } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import cloudIcon from '../../../../public/images/cloudsGrey.png';
+import cloudLeft from '../../../../public/images/cloudLeft.png'
+import cloudRight from '../../../../public/images/cloudRight.png'
 import styles from './BlockCloudContainer.module.scss';
 
 interface BlockCloudContainerProps {
@@ -23,15 +25,22 @@ const BlockCloudContainer: React.FC<BlockCloudContainerProps & React.HTMLAttribu
     }) => {
     return (
         <Component className={`${styles.block__cloud__container} ${styles[version]}`} {...props}>
-            {Array.from({ length: cloudCount }).map((_, index) => (
+
+            {version === 'block' ? Array.from({ length: cloudCount }).map((_, index) => (
                 <Image
                     key={index}
                     src={img}
                     alt={`cloudIcon-${index}`}
                     width={width}
                 />
-            ))}
-            <p>{text}</p>
+            )) :
+                <div className={styles[version]}>
+                    <Image src={cloudLeft} alt={'cloudLeft'}/>
+                    <Image src={cloudRight} alt={'cloudRight'}/>
+                    <p className={`${styles[version]}__p`}>{text}</p>
+                </div>
+            }
+            {version === 'block' && <p className={styles[`${version}__p`]}>{text}</p>}
         </Component>
     );
 };
--- src/app/_components/RecommendedProducts/RecommendedProductsContent/RecommendedProductsContent.tsx
+++ src/app/_components/RecommendedProducts/RecommendedProductsContent/RecommendedProductsContent.tsx
@@ -4,16 +4,19 @@ import {getAboutContent} from "@/service/public/aboutContent";
 import Image from "next/image";
 import styles from './RecommendedProductsContent.module.scss'
 import Link from "next/link";
+import BlockCloudContainer from "@/app/_components/BlockCloudContainer/BlockCloudContainer";
+import RecommendedProductsContentLogoBLock
+    from "@/app/_components/RecommendedProducts/RecommendedProductsContent/RecommendedProductsContentLogoBLock/RecommendedProductsContentLogoBLock";
 const RecommendedProductsContent = async () => {
     const {category} = await getAboutContent()
     console.log(category,'categories')
     return (
         <div className="mainGrid">
-            {category?.map(({id,name,image}) => (
-                <Link href={`/category/${id}`} className={styles.recomended__products__content}>
-                    <Image className={styles.recomended__products__content__img} key={id} width={100} height={100} src={`${process.env.CATEGORY_IMG_API_URL}/${image}`}  alt={`${process.env.CATEGORY_IMG_API_URL}/${image}`}/>
+            {category?.map((item) => (
+                <div  key={item.id}  className={styles.recomended__products__content}>
+                    <RecommendedProductsContentLogoBLock  category={item}/>
+                </div>
 
-                </Link>
             ))}
         </div>
     );
